bc6cfc611f8c26c280022a4d94f3ce6176d1105f
Coffee448 <andy_may_hazi_mejoresamigas@hotmail.com>
2026-04-21 18:32:03 -0600

    Feeder uses FreeRTOS for a BlinkLED besides connecting as an Access Point

--- Automatic_Feeder/feeder/feeder.ino
+++ Automatic_Feeder/feeder/feeder.ino
@@ -1,27 +1,63 @@
-/*********
-  A. Joanellie H.A
+/*****************************
+  Author: A. Joanellie H.A
   Embedded Systems Engineer
-*********/
+ ****************************/
 #define output26 26
-// Load FreeRTOS library
+/*****************************
+ *        INCLUDES
+ ****************************/
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include "WiFi_comm.h"
 
-// Network credentials
+/*****************************
+ *    NETWORK CREDENTIALS
+ ****************************/
 const char* ssid     = "Automatic Feeder";
 const char* password = NULL;
 
+/*****************************
+ *      TASK HANDLES
+ ****************************/
+TaskHandle_t BlinkTaskHandle = NULL;
+
+/*****************************
+ *      ESP32 SETUP
+ ****************************/
 void setup() {
   Serial.begin(115200);
-  // Initialize the output variable as output
   pinMode(output26, OUTPUT);
-  // Set output to LOW
   digitalWrite(output26, LOW);
 
+/******  BLINK TASK ******/ 
   WiFi_comm_setup(ssid, password);
+  // Task creation for WiFi communication
+  xTaskCreatePinnedToCore(
+    BlinkTask,         // Task function
+    "BlinkTask",       // Task name
+    2048,             // Stack size (bytes)
+    NULL,              // Parameters
+    1,                 // Priority
+    &BlinkTaskHandle,  // Task handle
+    1                  // Core 1
+  );  
+
 }
 
 void loop(){
   WiFi_comm_task();
+}
+
+/*****************************
+ *    TASK FUNCTIONS
+ ****************************/
+void BlinkTask(void *parameter) {
+  for (;;) { // Infinite loop
+    Serial.println("PIN HIGH");
+    digitalWrite(output26, HIGH);
+    vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay for 10 ms
+    Serial.println("PIN LOW");
+    digitalWrite(output26, LOW);
+    vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay for 10 ms
+  }
 }
