75caac48dce62e785d67608ec1fb0ace787114da
Coffee448 <andy_may_hazi_mejoresamigas@hotmail.com>
2026-04-30 22:26:09 -0600

    ADDED: Timer, Callback. MODIFIED: Either Task and Callback print text on serial monitor

--- Automatic_Feeder/feeder/feeder.ino
+++ Automatic_Feeder/feeder/feeder.ino
@@ -17,9 +17,10 @@ const char* ssid     = "Automatic Feeder";
 const char* password = NULL;
 
 /*****************************
- *      TASK HANDLES
+ *      HANDLERS
  ****************************/
-TaskHandle_t BlinkTaskHandle = NULL;
+TaskHandle_t TaskHandle = NULL;
+TimerHandle_t TimerHandle = NULL;
 
 /*****************************
  *      ESP32 SETUP
@@ -29,21 +30,44 @@ void setup() {
   pinMode(output26, OUTPUT);
   digitalWrite(output26, LOW);
 
-/******  BLINK TASK ******/ 
+  /*****************************
+  *           TASK
+  ****************************/  
   WiFi_comm_setup(ssid, password);
   // Task creation for WiFi communication
   xTaskCreatePinnedToCore(
-    BlinkTask,         // Task function
-    "BlinkTask",       // Task name
-    2048,             // Stack size (bytes)
-    NULL,              // Parameters
-    1,                 // Priority
-    &BlinkTaskHandle,  // Task handle
-    1                  // Core 1
-  );  
+    Task,         // Task function
+    "Task",       // Task name
+    2048,         // Stack size (bytes)
+    NULL,         // Parameters
+    1,            // Priority
+    &TaskHandle,  // Task handle
+    1             // Core 1
+  ); 
+  if (TaskHandle == NULL) {
+    Serial.println("Failed to create task!");
+    while (1);
+  }
+  /*****************************
+  *           TIMER
+  ****************************/  
+  TimerHandle = xTimerCreate(
+    "Timer",               // Timer name
+    pdMS_TO_TICKS(5000),   // Timer period 
+    pdTRUE,                // Auto-reload
+    NULL,                  // Timer ID (not used)
+    Callback               // Callback function
+  );
+  if (TimerHandle == NULL) {
+    Serial.println("Failed to create timer!");
+    while (1);
+  }
+  xTimerStart(TimerHandle, 0); // Start the timer
 
 }
-
+/*****************************
+ *      ESP32 LOOP
+ ****************************/
 void loop(){
   WiFi_comm_task();
 }
@@ -51,13 +75,15 @@ void loop(){
 /*****************************
  *    TASK FUNCTIONS
  ****************************/
-void BlinkTask(void *parameter) {
+void Task(void *parameter) {
   for (;;) { // Infinite loop
-    Serial.println("PIN HIGH");
-    digitalWrite(output26, HIGH);
-    vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay for 10 ms
-    Serial.println("PIN LOW");
-    digitalWrite(output26, LOW);
-    vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay for 10 ms
+    Serial.println("Executing Task");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
+}
+/*****************************
+ *   CALLLBACK FUNCTIONS
+ ****************************/
+void Callback(TimerHandle_t xTimer) {
+  Serial.println("Callback executed");
 }
